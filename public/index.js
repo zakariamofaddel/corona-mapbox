a0dceae7dd7443402b4827cf3210f588cdc15b29
Zakaria Mofaddel <mofaddel.z@outlook.it>
2020-11-22 01:21:54 +0200

    App is finished, can add some little style changes in the future

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
+//this key doesn't work anymore. Visit https://www.mapbox.com/, create a new account and get an access token.
 const mapbox_token =
-	"pk.eyJ1IjoiemFrYXJpYTk3IiwiYSI6ImNraHFzZ3UxcjAzZTEycWxodHMyYzVzejMifQ.XnvDv4_zdedxUe9qtUF_dQ";
+	"pk.eyJ1IjoiemFrYXJpYTk3IiwiYSI6ImNraHM4cGxxeTQ4MDEyemw2emJleDQ4NGwifQ.syhzvBYxi7VjXx6uos19-w";
 
 mapboxgl.accessToken = mapbox_token;
 var map = new mapboxgl.Map({
@@ -9,16 +10,29 @@ var map = new mapboxgl.Map({
 	zoom: 2, // starting zoom
 });
 
+const getColorFromInfected = (count) => {
+	if (count < 2) {
+		return "green";
+	}
+
+	if (count >= 2 && count < 5) {
+		return "yellow";
+	}
+	if (count >= 5 && count < 8) {
+		return "orange";
+	}
+	if (count >= 8) {
+		return "red";
+	}
+};
+
 fetch("./data600.json")
 	.then((response) => response.json())
 	.then((data) => {
 		dataArray = data.data;
 
 		dataArray
-			.filter(
-				(report) =>
-					!report.invisible && (report.infected / report.pop) * 100 > 1
-			)
+			.filter((report) => !report.invisible)
 			.forEach((report) => {
 				const {
 					pop,
@@ -31,8 +45,28 @@ fetch("./data600.json")
 					name,
 				} = report;
 
-				let marker = new mapboxgl.Marker()
+				infectedPercentage = (infected / pop) * 100;
+
+				let marker = new mapboxgl.Marker({
+					color: getColorFromInfected(infectedPercentage),
+				})
 					.setLngLat([longitude, latitude])
-					.addTo(map);
+					.addTo(map)
+					.setPopup(
+						new mapboxgl.Popup().setHTML(`
+                    <ul>
+                        <li><h3>Country: ${country}</h3></li>
+                        <li><h3>Region: ${name}<h4></li>
+                        <li>Population: ${pop}</li>
+                        <li>Infected: ${infected}</li>
+                        <li>Percentage: ${infectedPercentage.toFixed(2)}%</li>
+                        <li>deaths: ${dead}</li>  
+                    </ul>`)
+					);
+
+				if (infectedPercentage < 0.5) {
+					marker.remove();
+					return;
+				}
 			});
 	});
